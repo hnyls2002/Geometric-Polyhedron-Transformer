3af260e5783eb5f57571b6aacce41b21548517f8
hnyls2002 <hnyls2002@gmail.com>
2023-06-04 22:08:00 +0800

    🔧 build(makefile and .sh): &&

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # get the name of the test file
 name=$1
-cp ./testcases/$name.c ./testspace/input.c
-cp ./testcases/$name.c.clay.scop ./testspace/result.scop
-./build/transformer ./testspace/input.c
-./build/codegen ./testspace/result.scop ./testspace/result.c
+cp ./testcases/$name.c ./testspace/input.c && \
+cp ./testcases/$name.c.clay.scop ./testspace/result.scop && \
+./build/transformer ./testspace/input.c && \
+./build/codegen ./testspace/result.scop ./testspace/result.c && \
 diff ./testspace/result.c ./testspace/output.c
